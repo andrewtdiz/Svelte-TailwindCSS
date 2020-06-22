dccc930480e79261247e32cb8aeb6aedd07eb371
andrewtdiz <andrewdizenzojhu@gmail.com>
2020-06-22 10:37:02 -0400

    Template

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Player from './Player.svelte'
+  import Tailwindcss from "./Tailwindcss.svelte";
+
+  let players = [
+    {
+      name: 'John',
+      score: 0,
+    },
+    {
+      name: 'Ann',
+      score: 5
+    }
+  ]
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+</style>
+
+<Tailwindcss />
+<main>
+  {#if players.length==0}
+    <p>No players</p>
+  {:else}
+    {#each players as player }
+      <Player name={player.name} score={player.score}/>
+    {/each}
+  {/if}
+  <button class="mt-6 px-4 py-2 bg-blue-500 text-white" on:click={() => {players = [...players, {name: 'New Player', score: 0}]}}>Add new player</button>
+</main>
---
+++ src/Player.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let name;
+  export let score
+  let showControls = false
+</script>
+
+<div>
+  <p> { name } </p>
+  <p> { score } </p>
+  {#if showControls}
+  <button class="px-4 py-1 bg-blue-500 text-white rouned" on:click={() => score += 1}> +1 </button>
+  <button class="px-4 py-1 bg-blue-500 text-white rouned" on:click={() => score -= 1}> -1 </button>
+  {/if}
+  <button on:click={() => showControls = !showControls }>{showControls ? 'Truth' : 'Falso'}</button>
+</div>
